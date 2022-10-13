a5803298f20a12b345f569138978648d72d0989b
Russell Graviet <russgrav@gmail.com>
2022-10-13 15:44:14 +0900

    Add new buttons, GitHub link, and styling

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const container = document.querySelector('.container');
 const gridSizer = document.querySelector('.grid-sizer')
-
-
+const rainbowButton = document.querySelector('.rainbow-button');
+const defaultButton = document.querySelector('.default-button');
+const eraserButton = document.querySelector('.eraser');
+const clearButton = document.querySelector('.clear');
 
 for (x = 0; x < 256; x++) {
   let gridSquare = document.createElement('div');
@@ -12,7 +14,6 @@ for (x = 0; x < 256; x++) {
     gridSquare.classList.add('gridColor');
   })
 }
-
 
 gridSizer.addEventListener('click', () => {
   do {
@@ -26,15 +27,59 @@ gridSizer.addEventListener('click', () => {
   }
 })
 
+defaultButton.addEventListener('click', () => {
+  const gridSquareList = document.querySelectorAll('.grid-square');
+  gridSquareList.forEach((square) => {
+    square.addEventListener('mouseover', () => {
+      square.style.backgroundColor = '#333333';
+    });
+  })
+})
+
+rainbowButton.addEventListener('click', () => {
+  const gridSquareList = document.querySelectorAll('.grid-square');
+  gridSquareList.forEach((square) => {
+    square.addEventListener('mouseover', () => {
+      square.style.backgroundColor = getRandomRGB();
+    });
+  })
+})
+
+eraserButton.addEventListener('click', () => {
+  const gridSquareList = document.querySelectorAll('.grid-square');
+  gridSquareList.forEach((square) => {
+    square.addEventListener('mouseover', () => {
+      square.style.backgroundColor = 'white';
+    });
+  })
+})
+
+clearButton.addEventListener('click', () => {
+  const gridSquareList = document.querySelectorAll('.grid-square');
+  gridSquareList.forEach((square) => {
+    square.style.backgroundColor = 'white';
+  })
+})
+
+function getRandomRGB() {
+  const randomBetween = (min, max) => min + Math.floor(Math.random() * 
+  (max - min + 1));
+  const r = randomBetween(0, 255);
+  const g = randomBetween(0, 255);
+  const b = randomBetween(0, 255);
+  const rgb = `rgb(${r},${g},${b})`;
+  return rgb;
+}
+
 function changeDimensions(sides) {
   let x = sides;
-  let widthAndHeight = 320 / sides;
+  let widthAndHeight = 400 / sides;
   console.log(widthAndHeight);
   const gridSquareList = document.querySelectorAll('.grid-square');
-  for (i = 0; i < gridSquareList.length; i++) {
-    const gridSquare = document.querySelector('.grid-square');
+  gridSquareList.forEach((square) => {
+    let gridSquare = document.querySelector('.grid-square');
     container.removeChild(gridSquare);
-  }
+  })
   for (i = 0; i < (x * x); i++) {
     let gridSquare = document.createElement('div');
     gridSquare.classList.add('grid-square');
